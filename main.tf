ab2288723e78eff82ea074ba3f1e54dfd2dc9d2b
uthmanakz <uthmanakinwumi1@gmail.com>
2024-11-01 19:50:29 +0000

    terraform init/plan

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "eu-west-2"
 }
 
-resource "aws_instance" {
+resource "aws_instance" "node-a" {
     ami = "ami-0acc77abdfc7ed5a6"
     instance_type = "t2.micro"
     key_name = "msowens-key"
